eaa09f8da9161c96eb84180e67f0ea526f42a7bc
Manpreet <manpreetkunnath@gmail.com>
2024-09-01 13:41:48 +0530

    Smoke Test: Add tcp server

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	listener, err := net.Listen("tcp", ":10000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	defer listener.Close()
 
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
+	log.Println("Server listening on :10000")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %v", err)
+			continue
+		}
+		go handleConnection(conn)
 	}
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	_, err := io.Copy(conn, conn)
+	if err != nil {
+		log.Printf("Failed to copy: %v", err)
+		return
+	}
+}
